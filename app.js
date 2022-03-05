c10f0dfd33541a9827792f9f7cd65295a1a2473f
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-05 15:41:11 -0800

    First request

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=bc4b2f778a340e20f3c280094bf0b198&query=37.8267,-122.4233'
 
-console.log('Stopping')
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
+
