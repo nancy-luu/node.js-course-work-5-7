8a2395511d42abea6f23a5417a7f2c4795e292c8
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-06 20:50:39 -0800

    HTTP request challenge

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=bc4b2f778a340e20f3c280094bf0b198&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=bc4b2f778a340e20f3c280094bf0b198&query=37.8267,-122.4233&units=f'
+
+// json:true automatically parses response.body
+request({ url: url, json: true }, (error, response) => {
+    // const data = JSON.parse(response.body)
+    // in url set key value pair units = f to convert default c to f
+    console.log((response.body.current.weather_descriptions[0]) + ': It is currently ' + (response.body.current.temperature) + '° out. It feels like ' + (response.body.current.feelslike) + '° out.')
+})
+
+// Geocoding
+// Address => Lat/Long => Weather
 
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoibmx1dWFyY2giLCJhIjoiY2t3cW1zbmg5MG9hNjJuczZ3dXk1ZWp4NSJ9.bMisuCoRnMAsWfSoRzhraw&limit=1'
+
+request({ url: geocodeURL, json:true }, (error, response) => {
+    console.log('lat: ' + response.body.features[0].center[1])
+    console.log('long: ' + response.body.features[0].center[0])
 })
 
