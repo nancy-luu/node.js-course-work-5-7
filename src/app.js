60acbcdd3bcd939c42480ff00c3cc02eb248fb69
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-08 11:03:42 -0800

    setting up HBS

--- src/app.js
+++ src/app.js
@@ -5,12 +5,6 @@ const express = require('express')
 // console.log(path.join(__dirname, '../public'))
 // console.log(path.join(__dirname, '../public/about.html'))
 
-
-const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
-
-app.use(express.static(publicDirectoryPath))
-
 // .get lets us configure what the server shuold do
 // app.get('', (req, res) => {
 //     res.send('<h1>WEATHER</h1>')
@@ -30,6 +24,34 @@ app.use(express.static(publicDirectoryPath))
 // app.get('/about', (req, res) => {
 //     res.send('<h1>ABOUT</h1>')
 // })
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    // render one of our views, express is configured to use the view engine hbs 
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Nancy Pantz'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me:',
+        name: 'Nancy Pantz'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'To find help - follow these links:'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({ 
